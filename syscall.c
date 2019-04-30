fa31791fbc30c6ccd08558b99fb75ca357966579
AndresSaVi <a01632107@itesm.mx>
2019-04-30 00:27:57 +0000

    practica04

--- syscall.c
+++ syscall.c
@@ -174,7 +174,7 @@ syscall(void)
   };
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    cprintf(" %s -> %d \n ", syscall_id[num] , num );
+  //  cprintf(" %s -> %d \n ", syscall_id[num] , num );
 	curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
